b8e4077f6d20cd1884233391c47c023412b0fbe0
Matheus Rosa <matheusrosataxa@gmail.com>
2024-07-24 14:46:56 -0300

    feat: req. 2 - class User

--- news/models.py
+++ news/models.py
@@ -6,3 +6,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    email = models.EmailField(max_length=200, null=False, blank=False)
+    password = models.CharField(max_length=200, null=False, blank=False)
+    role = models.CharField(
+        max_length=200, null=False, blank=False, default="default_role"
+    )
+
+    def __str__(self):
+        return self.name
