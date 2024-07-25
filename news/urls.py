081073ee88fdcbef7a45f3238eb14e7e3d56c0e6
Matheus Rosa <matheusrosataxa@gmail.com>
2024-07-24 22:48:21 -0300

    feat: views, urls - news_detail

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from news.views import home_page
+from news.views import home_page, news_detail
 
 
 urlpatterns = [
     path("", home_page, name="home-page"),
+    path("news/<int:news_id>", news_detail, name="news-detail"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from news.models import News
 
@@ -8,3 +8,8 @@ from news.models import News
 def home_page(request):
     news_list = News.objects.all()
     return render(request, "home.html", {"news_list": news_list})
+
+
+def news_detail(request, news_id):
+    news = get_object_or_404(News, pk=news_id)
+    return render(request, "news_detail.html", {"news": news})
